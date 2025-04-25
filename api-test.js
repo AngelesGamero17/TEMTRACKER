9029677c8b77e2cda3dd7c69a03a98622f4613ea
AngelesGamero17 <angelesgamero17@outlook.com>
2025-04-25 12:27:59 -0500

    seis

--- api-test.js
+++ api-test.js
@@ -1,7 +1,11 @@
-const claveApi = '427bf53c89c64c22833115745252504' ; 
+const claveApi = '427bf53c89c64c22833115745252504'; 
 const idioma = 'es';
-const cuidad = 'huancayo';
+const ciudad = 'huancayo'; // corregido el nombre de la variable
 
-const apiClimaActual = ''
+const apiClimaActual = `https://api.weatherapi.com/v1/current.json?q=${ciudad}&lang=${idioma}&key=${claveApi}`;
 
-con
+const response = await fetch(apiClimaActual); // esto solo funcionará si estás dentro de una función async o en un entorno que lo permita
+let data = await response.json();
+
+console.log(data.location);
+
